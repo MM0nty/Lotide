e9d3d71f718ae31934792784bd25791f3c38a638
M <79332342+MM0nty@users.noreply.github.com>
2021-07-19 21:29:44 +0000

    Assert equal function

---
+++ AssertEqual.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected)
+{
+  if (actual === expected)
+  {
+    console.log("✅ Assertion passed: " + actual + " === " + expected)
+  }
+  else
+  {
+    console.log("❌ Assertion failed: " + actual + " !== " + expected)
+  }
+}
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("String", "string");
+assertEqual(0, 3);
+
+/*
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ Assertion passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌ Assertion failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("String", "string");
+assertEqual(0, 3);
+*/
