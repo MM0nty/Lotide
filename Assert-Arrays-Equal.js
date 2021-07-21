b2087ee4429ae59946d171b533e4044ccf0b07bf
M <79332342+MM0nty@users.noreply.github.com>
2021-07-21 00:33:26 +0000

    Merged Assert-Equals and Equal-Arrays

--- Assert-Arrays-Equal.js
+++ Assert-Arrays-Equal.js
@@ -1,29 +1,21 @@
-const equalArrays = function(array1, array2)
+const assertArraysEqual = function(array1, array2)
 {
-  for (i = 0; i < array1.length; i++)
-  {
-    for (j = 0; j < array2[i].length; j++)
-    {
-      if (array1[i] === array2[j])
-      {
-        true;
-      }
-      else
-      {
-        false;
-      }
-    }
-  }
-};
-
-const assertEqual = function(actual, expected)
-{
-  if (actual === expected)
-  {
-    console.log(`✅ Assertion passed: ${actual} === ${expected}`);
-  }
-  else
+  if (array1.length !== array2.length)
   {
     console.log(`❌ Assertion failed: ${actual} !== ${expected}`);
+    return false;
   }
+  for (i = 0; i < array1.length; i++)
+  {
+    if (array1[i] !== array2[i])
+    {
+      console.log("❌ Assertion false");
+      return false;
+    }
+  }
+  console.log("✅ Assertion true");
+  return true;
 };
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
