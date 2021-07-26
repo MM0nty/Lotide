7fec09d6fbcc737d3aeba84e5eaabd7ccdeac65b
M <79332342+MM0nty@users.noreply.github.com>
2021-07-26 20:49:16 +0000

    Index module

--- Index.js
+++ Index.js
@@ -1,10 +1,16 @@
 const head = require("./Head");
 const tail = require("./Tail");
 const middle = require("./Middle");
+const assertEqual = require("./Assert-Equal");
+const equalArrays = require("./Equal-Arrays");
+const assertArraysEqual = require("./Assert-Arrays-Equal");
 
 module.exports =
 {
   head: head,
   tail: tail,
-  middle: middle
+  middle: middle,
+  assertEqual: assertEqual,
+  equalArrays: equalArrays,
+  assertArraysEqual: assertArraysEqual
 };
