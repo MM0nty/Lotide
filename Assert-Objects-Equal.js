44e4005d4c1f16fe2b64ae5ea0971c41fdea9f62
MM0nty <79332342+MM0nty@users.noreply.github.com>
2021-07-30 20:34:52 -0400

    Update Assert-Objects-Equal.js

--- Assert-Objects-Equal.js
+++ Assert-Objects-Equal.js
@@ -16,23 +16,4 @@ const assertObjectsEqual = function(actual, expected)
   }
 }
 
-const assertArraysEqual = function(array1, array2)
-{
-  if (array1.length !== array2.length)
-  {
-    console.log(`❌ Assertion failed: ${actual} !== ${expected}`);
-    return false;
-  }
-  for (i = 0; i < array1.length; i++)
-  {
-    if (array1[i] !== array2[i])
-    {
-      console.log("❌ Assertion false");
-      return false;
-    }
-  }
-  console.log("✅ Assertion true");
-  return true;
-};
-
 module.exports = assertObjectsEqual;
