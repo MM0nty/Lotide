6be0d6fdfbb81202a9e45b13e0783b49b95b8801
M <79332342+MM0nty@users.noreply.github.com>
2021-08-17 18:15:38 +0000

    Fixed syntax

--- EqualArrays.js
+++ EqualArrays.js
@@ -1,15 +1,11 @@
 const assertEqual = require("./AssertEqual");
 
-const equalArrays = function(array1, array2)
-{
-  if (array1.length !== array2.length)
-  {
+const equalArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
     return false;
-  }
-  for (i = 0; i < array1.length; i++)
-  {
-    if (array1[i] !== array2[i])
-    {
+  };
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
       return false;
     }
   }
