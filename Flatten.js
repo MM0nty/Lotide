093d4e61ffb8d19bf9b4606bcf803674e47ca4cd
M <79332342+MM0nty@users.noreply.github.com>
2021-08-17 21:41:34 +0000

    Implemented flatten function

---
+++ Flatten.js
@@ -0,0 +1,15 @@
+const flatten = function(array) {
+  const flattened = [];
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      for (let i = 0; i < item.length; i++) {
+        flattened.push(item[i]);
+      }
+    } else {
+      flattened.push(item);
+    }
+  });
+  return console.log(flattened);
+};
+
+flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
