8f7ab54941f7093632cfc359a955dac4d8b1731b
M <79332342+MM0nty@users.noreply.github.com>
2021-08-17 20:08:34 +0000

    Fixed syntax

--- Without.js
+++ Without.js
@@ -1,19 +1,16 @@
-const without = function(source, toRemove)
-{
-  let filtered = [];
-  for (i = 0; i < source.length; i++)
-  {
-    if (!toRemove.includes(source[i]))
-    {
+const without = function(source, toRemove) {
+  const filtered = [];
+  for (let i = 0; i < source.length -1; i++) {
+    if (!toRemove.includes(source[i])) {
       filtered.push(source[i]);
     }
   }
   console.log(`Original: ${source}`);
-  console.log(`Filtered: ${filtered}`);
+  return console.log(filtered);
 };
 
 without([1, 2, 3], [1]); // [2, 3]
 without([1, 2, 3], [1, 2]); // [3]
 without([3, 4, 5], [4, 5]); // [3]
 without(["1", "2", "3"], [1, 2, "3"]); // ["1", "2"]
-console.log(without(["hello", "world", "lighthouse"], ["lighthouse"]));
+without(["hello", "world", "lighthouse"], ["lighthouse"]);
