a531718b148e4ad17a36c6b6936016d4208f5152
M <79332342+MM0nty@users.noreply.github.com>
2021-08-19 17:30:59 +0000

    Fixed syntax (indentations)

--- FindKey.js
+++ FindKey.js
@@ -1,12 +1,9 @@
 const assertEqual = require("./AssertEqual");
 
-const findKey = function(object, callback)
-{
-  for (key of Object.keys(object))
-  {
-    let value = object[key];
-    if (callback(value))
-    {
+const findKey = function(object, callback) {
+  for (const key of Object.keys(object)) {
+    const value = object[key];
+    if (callback(value)) {
       console.log(key);
       return key;
     }
@@ -14,8 +11,8 @@ const findKey = function(object, callback)
   return (console.log(undefined));
 }
 
-assertEqual(findKey);
-
+// assertEqual(findKey);
+assertEqual(
 findKey({
   "Blue Hill": {stars: 1},
   "Akaleri": {stars: 3},
@@ -23,4 +20,5 @@ findKey({
   "elBulli": {stars: 3},
   "Ora": {stars: 2},
   "Akelarre": {stars: 3}
-}, x => x.stars === 2);
+}, x => x.stars === 2)
+, "noma");
