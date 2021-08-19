08e78bf7a63667759419c4033ff059834fc49fe8
M <79332342+MM0nty@users.noreply.github.com>
2021-08-19 01:56:13 +0000

    Corrected implemented EqualObjects

--- EqualObjects.js
+++ EqualObjects.js
@@ -1,37 +1,28 @@
 const assertEqual = require("./AssertEqual");
 const equalArrays = require("./EqualArrays");
 
-const equalObjects = function(object1, object2)
-{
-  if (Object.keys(object1).length !== Object.keys(object2).length)
-  {
-    console.log("False, not same lengths");
-    return false;
-  }
-  else
-  {
-    for (key of Object.keys(object1))
-    {
-      if (Array.isArray(Object.keys(object1)) === true && Array.isArray(Object.keys(object2)) === true)
-      {
-        equalArrays(object1, object2);
+const equalObjects = function (object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return console.log("False, not same lengths", false);
+  } else {
+    for (key of keys1) {
+      if (!object2[key]) { //if object2 has same key
+        return console.log("Found unmatched value", false);
       }
-      else if (object2[key])
-      {
-        if (object1[key] === object2[key])
-        {
-          console.log("True");
+      if (Array.isArray(keys1) === true && Array.isArray(keys2) === true) {
+        equalArrays(object1[key], object2[key]);
+        if (equalArrays(object1[key], object2[key]) === false) {
+          return console.log("Found unmatched array", false);
         }
+      } else if (object1[key] !== object2[key]) {
+        return console.log("Found unmatched value", false);
       }
-      else
-      {
-        console.log("Found unmatched value");
-        return false;
-      }
-      return true;
     }
   }
-}
+  return console.log("End", true);
+};
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
@@ -39,5 +30,12 @@ equalObjects(ab, ba); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
 equalObjects(ab, abc); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+equalObjects(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+equalObjects(cd, cd2); // => false
 
 module.exports = equalObjects;
